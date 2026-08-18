becf48797c18e57e552f861121bbbc7f1f6105ed
Sziloyoo <sziloyoo@gmail.com>
2026-08-18 16:15:35 +0200

    chore: setup project configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <>
-      <h1>Yoo</h1>
+      <div className="grid place-items-center h-screen">
+        <h1 className="text-3xl font-bold">Yoo!</h1>
+        <button
+          className="bg-indigo-100 p-1 mbg-indigo-50 hover:bg-indigo-200 text-black font-semibold py-2 px-4 rounded-lg transition-colors duration-150"
+          onClick={() => setCount(count + 1)}
+        >
+          count: {count}
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
